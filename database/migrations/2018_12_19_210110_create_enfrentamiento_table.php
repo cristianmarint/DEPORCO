75aab706f1f6d8ec39a589082da2a50cc526d078
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-01-03 15:30:33 -0500

    EL DAÑO D:

--- database/migrations/2018_12_19_210110_create_enfrentamiento_table.php
+++ database/migrations/2018_12_19_210110_create_enfrentamiento_table.php
@@ -23,15 +23,15 @@ class CreateEnfrentamientoTable extends Migration
             $table->timestamps();
 
             $table->foreign('calendario_id')->references('id_calendario')->on('calendario')
-                  ->onUpdate('cascade')->onDelete('restrict');
+                  ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('inscripcion_equipo_local_id')->references('id_inscripcion')->on('inscripcion')
-                  ->onUpdate('cascade')->onDelete('restrict');
+                  ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('inscripcion_equipo_visitante_id')->references('id_inscripcion')->on('inscripcion')
-                  ->onUpdate('cascade')->onDelete('restrict');
+                  ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('lugar_id')->references('id_lugar')->on('lugar')
-                  ->onUpdate('cascade')->onDelete('restrict');
+                  ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('usuario_id')->references('id')->on('users')
-                  ->onUpdate('cascade')->onDelete('restrict');
+                  ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -42,6 +42,23 @@ class CreateEnfrentamientoTable extends Migration
      */
     public function down()
     {
+      // Schema::table('enfrentamiento',function(Blueprint $table){
+      //       $table->dropForeign(['calendario_id']);
+      //       $table->dropColumn('calendario_id');
+
+      //       $table->dropForeign(['inscripcion_equipo_local_id']);
+      //       $table->dropColumn('inscripcion_equipo_local_id');
+
+      //       $table->dropForeign(['inscripcion_equipo_visitante_id']);
+      //       $table->dropColumn('inscripcion_equipo_visitante_id');
+
+      //       $table->dropForeign(['lugar_id']);
+      //       $table->dropColumn('lugar_id');
+            
+      //       $table->dropForeign(['usuario_id']);
+      //       $table->dropColumn('usuario_id');
+      //   });
         Schema::dropIfExists('enfrentamiento');
+      //   Schema::drop('enfrentamiento');
     }
 }
