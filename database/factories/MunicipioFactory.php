9a3ad2d4aa15b63b8244cad39dff3c13b1bda032
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-01 09:43:47 -0500

    cambios menores a factories

--- database/factories/MunicipioFactory.php
+++ database/factories/MunicipioFactory.php
@@ -9,7 +9,7 @@ $factory->define(Municipio::class, function (Faker $faker) {
     $countDepartamento = Departamento::count();
 
     return [
-        'nombre' => $faker->name,
+        'nombre' => $faker->city,
         'departamento_id' => rand(1,$countDepartamento)
     ];
 });
