79ab98eb1430d0ac9f156b7a09e819e8209a6335
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-01 11:16:26 -0500

    worth it!

--- database/seeds/DatosBasicosTableSeeder.php
+++ database/seeds/DatosBasicosTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\DatosBasicos;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class DatosBasicosTableSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatosBasicosTableSeeder extends Seeder
     {
         DatosBasicos::create(array(
             'cedula' => '1112112112',
+            'foto' => 'http://lorempixel.com/800/800/people/',
             'telefono_id' => \App\Telefono::all()->random()->id,
             'primer_nombre' => 'Cristian',
             'segundo_nombre' => 'Alexander',
@@ -25,6 +27,20 @@ class DatosBasicosTableSeeder extends Seeder
             'direccion_id' => \App\Direccion::all()->random()->id,
             'eps_id' => \App\Eps::all()->random()->id,
             'email' => 'cristianmarint@cotecnova.edu.co'
+        ));
+
+        DatosBasicos::create(array(
+            'cedula' => '222111222',
+            'foto' => 'http://lorempixel.com/800/800/people/',
+            'telefono_id' => \App\Telefono::all()->random()->id,
+            'primer_nombre' => 'Edwin',
+            'primer_apellido' => 'Lopez',
+            'tipo_sangre_id' => \App\TipoSangre::all()->random()->id,
+            'municipio_id' => \App\Municipio::all()->random()->id,
+            'genero_id' => 1,
+            'direccion_id' => \App\Direccion::all()->random()->id,
+            'eps_id' => \App\Eps::all()->random()->id,
+            'email' => 'edwin.lopezb.1297@cotecnova.edu.co'
         ));
 
         factory(DatosBasicos::class, 100)->create();
