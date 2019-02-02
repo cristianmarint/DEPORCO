b48da9e99454952cc3af747c5a8966ed21247fc6
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-02 18:48:42 -0500

    un poquito de sason

--- database/factories/DatosBasicosFactory.php
+++ database/factories/DatosBasicosFactory.php
@@ -4,8 +4,13 @@ use App\DatosBasicos;
 use Faker\Generator as Faker;
 
 $factory->define(DatosBasicos::class, function (Faker $faker) {
+    $proveedores = ['https://loremflickr.com/400/400/face,woman','https://loremflickr.com/400/400/face,man','https://loremflickr.com/400/400/player,man',
+                    'https://loremflickr.com/400/400/player,soccer','https://loremflickr.com/400/400/player,football','https://loremflickr.com/400/400/player,sport',
+                    'https://loremflickr.com/400/400/player,tennis','https://loremflickr.com/400/400/person'
+                   ];
     return [
         'cedula' => $faker->unixTime,
+        'foto'=> $proveedores[array_rand($proveedores)],
         'telefono_id' => \App\Telefono::all()->random()->id,
         'primer_nombre' => $faker->firstName,
         'segundo_nombre' => $faker->firstName,
