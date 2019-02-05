264a377fae6f65ebdca94c969dea732e426dbb32
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-05 13:21:19 -0500

    softdelete

--- app/Arbitro.php
+++ app/Arbitro.php
@@ -4,9 +4,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Arbitro extends Model
 {
+    use SoftDeletes;
+
     protected $table = "arbitro";
 
 
@@ -14,4 +17,8 @@ class Arbitro extends Model
         'datos_basicos_id',
         'user_id'
     ];
+
+    protected function datos_basicos(){
+        return $this->belongsTo(DatosBasicos::class);
+    }
 }
