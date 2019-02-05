ce9d526340a5720ba532b6c577a9230df0829c65
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-05 11:12:50 -0500

    view categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -10,9 +10,13 @@ namespace App;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Categoria extends Model
 {
+    use SoftDeletes;
+
     protected $table = "categoria";
 
     protected $fillable = [
---
+++ resources/views/categoria/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+@section('title','Categorias')
+@section('content')
+
+    <!-- Breadcrumb-->
+    <div class="breadcrumb-holder container-fluid">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
+            <li class="breadcrumb-item active"><a href="{{url('/categorias')}}">Categorias</a></li>
+            <li class="breadcrumb-item">Ver</li>
+        </ul>
+    </div>
+
+    <!-- Dashboard Counts Section-->
+    <section class="dashboard-counts">
+        <div class="container-fluid">
+            <div class="bg-white has-shadow">
+
+                <div class="form-group row">
+                    <label for="cedula" class="form-control-label col-sm-3 ">Descripcion</label>
+                    <div class="col-sm-9">
+                        <input id="cedula" type="text" name="cedula" class="form-control" value="{{ $categorias->descripcion }}" readonly>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+@endsection
