fcb1c5cc6e22d066ae9317b395bc4075f9b454b3
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-06 11:07:10 -0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,9 +39,7 @@ class HomeController extends Controller
                             // ->count('arbitro_id')
                             ;
         // SELECT arbitro_id,count(id) from enfrentamiento_arbitro group by arbitro_id;
-
-        $numero_partidos = DB::statement("SELECT arbitro_id,count(id) AS numero from enfrentamiento_arbitro group by arbitro_id;");
         
-        return view('home', compact('enfrentamientos','numero_partidos'));
+        return view('home', compact('enfrentamientos'));
     }
 }
