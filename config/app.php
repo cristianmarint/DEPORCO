71efea1e7057d7f4f5af5ae03ccc745c4c039294
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-13 18:26:06 -0500

    service provider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        TCG\Voyager\VoyagerServiceProvider::class,
 
         /*
          * Application Service Providers...
