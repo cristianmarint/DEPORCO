0bf1293066c02c078fa3115b2865c072566f9338
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-18 16:48:27 -0500

    Se movio el login con github de /login a /admin/login

---
+++ resources/views/vendor/voyager/login.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}" @if (config('voyager.multilingual.rtl')) dir="rtl" @endif>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="none" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="admin login">
+    <title>test Admin - {{ Voyager::setting("admin.title") }}</title>
+    <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
+    @if (config('voyager.multilingual.rtl'))
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css">
+        <link rel="stylesheet" href="{{ voyager_asset('css/rtl.css') }}">
+    @endif
+    <style>
+        body {
+            background-image:url('{{ Voyager::image( Voyager::setting("admin.bg_image"), voyager_asset("images/bg.jpg") ) }}');
+            background-color: {{ Voyager::setting("admin.bg_color", "#FFFFFF" ) }};
+        }
+        body.login .login-sidebar {
+            border-top:5px solid {{ config('voyager.primary_color','#22A7F0') }};
+        }
+        @media (max-width: 767px) {
+            body.login .login-sidebar {
+                border-top:0px !important;
+                border-left:5px solid {{ config('voyager.primary_color','#22A7F0') }};
+            }
+        }
+        body.login .form-group-default.focused{
+            border-color:{{ config('voyager.primary_color','#22A7F0') }};
+        }
+        .login-button, .bar:before, .bar:after{
+            background:{{ config('voyager.primary_color','#22A7F0') }};
+        }
+    </style>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+</head>
+<body class="login">
+<div class="container-fluid">
+    <div class="row">
+        <div class="faded-bg animated"></div>
+        <div class="hidden-xs col-sm-7 col-md-8">
+            <div class="clearfix">
+                <div class="col-sm-12 col-md-10 col-md-offset-2">
+                    <div class="logo-title-container">
+                        <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
+                        @if($admin_logo_img == '')
+                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
+                        @else
+                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
+                        @endif
+                        <div class="copy animated fadeIn">
+                            <h1>{{ Voyager::setting('admin.title', 'Voyager') }}</h1>
+                            <p>{{ Voyager::setting('admin.description', __('voyager::login.welcome')) }}</p>
+                        </div>
+                    </div> <!-- .logo-title-container -->
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-5 col-md-4 login-sidebar">
+
+            <div class="login-container">
+
+                <p>{{ __('voyager::login.signin_below') }}</p>
+
+                <form action="{{ route('voyager.login') }}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group form-group-default" id="emailGroup">
+                        <label>{{ __('voyager::generic.email') }}</label>
+                        <div class="controls">
+                            <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
+                         </div>
+                    </div>
+
+                    <div class="form-group form-group-default" id="passwordGroup">
+                        <label>{{ __('voyager::generic.password') }}</label>
+                        <div class="controls">
+                            <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}" class="form-control" required>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-block login-button">
+                        <span class="signingin hidden"><span class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
+                        <span class="signin">{{ __('voyager::generic.login') }}</span>
+                    </button>
+
+
+                    {{-- Login con Github Socialite --}}
+                    <a href="{{route('oauth.login','github')}}" class="btn btn-outline-secondary">
+                            <span >
+                                    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <g transform="translate(-614.000000, -553.000000)" fill="#000000" fill-rule="nonzero">
+                                                <g id="github" transform="translate(614.000000, 553.000000)">
+                                                    <path d="M12,0.297 C5.37,0.297 0,5.67 0,12.297 C0,17.6 3.438,22.097 8.205,23.682 C8.805,23.795 9.025,23.424 9.025,23.105 C9.025,22.82 9.015,22.065 9.01,21.065 C5.672,21.789 4.968,19.455 4.968,19.455 C4.422,18.07 3.633,17.7 3.633,17.7 C2.546,16.956 3.717,16.971 3.717,16.971 C4.922,17.055 5.555,18.207 5.555,18.207 C6.625,20.042 8.364,19.512 9.05,19.205 C9.158,18.429 9.467,17.9 9.81,17.6 C7.145,17.3 4.344,16.268 4.344,11.67 C4.344,10.36 4.809,9.29 5.579,8.45 C5.444,8.147 5.039,6.927 5.684,5.274 C5.684,5.274 6.689,4.952 8.984,6.504 C9.944,6.237 10.964,6.105 11.984,6.099 C13.004,6.105 14.024,6.237 14.984,6.504 C17.264,4.952 18.269,5.274 18.269,5.274 C18.914,6.927 18.509,8.147 18.389,8.45 C19.154,9.29 19.619,10.36 19.619,11.67 C19.619,16.28 16.814,17.295 14.144,17.59 C14.564,17.95 14.954,18.686 14.954,19.81 C14.954,21.416 14.939,22.706 14.939,23.096 C14.939,23.411 15.149,23.786 15.764,23.666 C20.565,22.092 24,17.592 24,12.297 C24,5.67 18.627,0.297 12,0.297" id="Shape"></path>
+                                                </g>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </span>
+                    </a>
+
+              </form>
+
+              <div style="clear:both"></div>
+
+              @if(!$errors->isEmpty())
+              <div class="alert alert-red">
+                <ul class="list-unstyled">
+                    @foreach($errors->all() as $err)
+                    <li>{{ $err }}</li>
+                    @endforeach
+                </ul>
+              </div>
+              @endif
+
+            </div> <!-- .login-container -->
+
+        </div> <!-- .login-sidebar -->
+    </div> <!-- .row -->
+</div> <!-- .container-fluid -->
+<script>
+    var btn = document.querySelector('button[type="submit"]');
+    var form = document.forms[0];
+    var email = document.querySelector('[name="email"]');
+    var password = document.querySelector('[name="password"]');
+    btn.addEventListener('click', function(ev){
+        if (form.checkValidity()) {
+            btn.querySelector('.signingin').className = 'signingin';
+            btn.querySelector('.signin').className = 'signin hidden';
+        } else {
+            ev.preventDefault();
+        }
+    });
+    email.focus();
+    document.getElementById('emailGroup').classList.add("focused");
+
+    // Focus events for email and password fields
+    email.addEventListener('focusin', function(e){
+        document.getElementById('emailGroup').classList.add("focused");
+    });
+    email.addEventListener('focusout', function(e){
+       document.getElementById('emailGroup').classList.remove("focused");
+    });
+
+    password.addEventListener('focusin', function(e){
+        document.getElementById('passwordGroup').classList.add("focused");
+    });
+    password.addEventListener('focusout', function(e){
+       document.getElementById('passwordGroup').classList.remove("focused");
+    });
+
+</script>
+</body>
+</html>
