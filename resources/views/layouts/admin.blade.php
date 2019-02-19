06302805d9705f0c5cd3e266da23fb5fccc6ad26
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-02-19 13:45:38 -0500

    cambios de ruta de archivos a ´/storage/*´

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,12 +23,12 @@
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="{{url('css/custom.css')}}">
     <!-- Favicon-->
-    <link rel="icon" type="image/png" href="{{url('img/favicon/cotecnova.png')}}">
+    <link rel="icon" type="image/png" href="{{url('storage/img/favicon/cotecnova.png')}}">
 </head>
 <body>
     {{-- <!-- LOADER --> --}}
     {{-- <div id="preloader">
-           <img class="preloader" src="{{ url('img/Loading-Page/football_loader.gif') }}" alt="">
+           <img class="preloader" src="{{ url('storage/img/Loading-Page/football_loader.gif') }}" alt="">
     </div> --}}
 
     <div class="page">
@@ -110,7 +110,7 @@
             <nav class="side-navbar">
                 <!-- Sidebar Header-->
                 <div class="sidebar-header d-flex align-items-center">
-                    <div class="avatar"><img src="{{url('img/favicon/cotecnova.png')}}" alt="..." class="img-fluid rounded-circle"></div>
+                    <div class="avatar"><img src="{{url('storage/img/favicon/cotecnova.png')}}" alt="..." class="img-fluid rounded-circle"></div>
                     <div class="title">
                         <h1 class="h4">
                             {{-- {{ Auth::user()->datos_basicos->primer_nombre }} {{ Auth::user()->datos_basicos->primer_apellido }} --}}
