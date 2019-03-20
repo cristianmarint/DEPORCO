9afd7313386e9ec4076a2598a627ab1f544834ff
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-03-20 16:29:42 -0500

    Blades de enfrentamiento

---
+++ resources/views/enfrentamiento/create.blade.php
@@ -0,0 +1,232 @@
+@extends('layouts.admin')
+@section('title','Instituciones')
+@section('content')
+    <!-- Breadcrumb-->
+    <div class="breadcrumb-holder container-fluid">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
+            <li class="breadcrumb-item active"><a href="{{url('institutos')}}">Instituciones</a></li>
+            <li class="breadcrumb-item">Crear</li>
+        </ul>
+    </div>
+    
+    @if(Session::has('error'))
+        <script>
+            setTimeout(function(){
+                swal("¡Algo ha salido mal!", "Ha habido un error al procesar la peticion, vuelte a intentarlo", "error");
+            }, 500);
+        </script>
+    @endif
+
+    <!-- Dashboard Counts Section-->
+    <section class="dashboard-counts">
+        <div class="container-fluid">
+            <div class="bg-white has-shadow">
+                <!-- Form Elements -->
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{route('institutos.store')}}" method="POST" enctype="multipart/form-data" class="form-horizontal">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="create_nit" class="form-control-label col-sm-3 ">Nit</label>
+                                <div class="col-sm-9">
+                                    <input id="create_nit" type="text" name="nit" class="form-control {{ $errors->has('nit') ? ' is-invalid' : '' }}" value="{{old('nit')}}">
+                                    @if ($errors->has('nit'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('nit') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="create_codigo_dane" class="form-control-label col-sm-3">Codigo Dane      </label>
+                                <div class="col-sm-9">
+                                    <input id="create_codigo_dane" type="text" name="codigo_dane" class="form-control{{ $errors->has('codigo_dane') ? ' is-invalid' : '' }}" value="{{old('codigo_dane')}}">
+                                    @if ($errors->has('codigo_dane'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('codigo_dane') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="create_nombre" class="form-control-label col-sm-3">Nombre Instituci&oacute;n      </label>
+                                <div class="col-sm-9">
+                                    <input id="create_nombre" type="text" name="nombre" class="form-control{{ $errors->has('nombre') ? ' is-invalid' : '' }}" value="{{old('nombre')}}">
+                                    @if ($errors->has('nombre'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('nombre') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+                            <div class="form-group row">
+                                <label for="create_logo" class="col-sm-3 form-control-label">Logo/escudo</label>
+                                <div class="col-sm-9">
+                                    <input id="create_logo" type="file" name="logo" class="form-control{{ $errors->has('logo') ? ' is-invalid' : '' }}" accept="image/*">
+                                    @if ($errors->has('logo'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('logo') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+                            <div class="form-group row">
+                                <label for="create_departamento" class="col-sm-3 form-control-label">Departamento</label>
+                                <div class="col-sm-9">
+                                    <select name="departamento" id="create_departamento" class="form-control{{ $errors->has('departamento') ? ' is-invalid' : '' }}">
+                                        <option value="0" >Seleccione un departamento</option>
+                                        @foreach($departamentos as $departamento)
+                                            <option value="{{$departamento->id}}">{{$departamento->nombre}}</option>
+                                        @endforeach
+                                    </select>
+                                    @if ($errors->has('departamento'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('departamento') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="create_municipio" class="col-sm-3 form-control-label">Municipio / Ciudad</label>
+                                <div class="col-sm-9">
+                                    <select name="municipio" id="create_municipio" class="form-control{{ $errors->has('municipio') ? ' is-invalid' : '' }}">
+                                        <option value="0" >Seleccione un Municipio / Ciudad</option>
+                                    </select>
+                                    @if ($errors->has('municipio'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('municipio') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="create_tipo_educacion" class="col-sm-3 form-control-label">Tipo de Educacion</label>
+                                <div class="col-sm-9">
+                                    <select name="tipo_educacion" id="create_tipo_educacion" class="form-control{{ $errors->has('tipo_educacion') ? ' is-invalid' : '' }}">
+                                        <option value="0" >Seleccione un tipo</option>
+                                        @foreach($tiposEducacion as $tipoEducacion)
+                                            <option value="{{$tipoEducacion->id}}">{{$tipoEducacion->tipo}}</option>
+                                        @endforeach
+                                    </select>
+                                    @if ($errors->has('tipo_educacion'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('tipo_educacion') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+                            <div class="form-group row">
+                                <label for="create_calle" class="form-control-label col-sm-3">Direccion</label>
+                                <div class="col-sm-4">
+                                    <input id="create_calle" type="text" name="calle" class="form-control{{ $errors->has('calle') ? ' is-invalid' : '' }}" value="{{old('calle')}}" placeholder="Calle">
+                                    @if ($errors->has('calle'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('calle') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                <div class="col-sm-3">
+                                    <input id="create_carrera" type="text" name="carrera" class="form-control{{ $errors->has('carrera') ? ' is-invalid' : '' }}" value="{{old('carrera')}}" placeholder="Carrera">
+                                    @if ($errors->has('carrera'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('carrera') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                <div class="col-sm-2">
+                                    <input id="create_numero" type="text" name="numero" class="form-control{{ $errors->has('numero') ? ' is-invalid' : '' }}" value="{{old('numero')}}" placeholder="Número">
+                                    @if ($errors->has('numero'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('numero') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-group row">
+
+                                <label for="create_tipo_telefono" class="col-sm-3 form-control-label">Telefono</label>
+                                <div class="col-sm-3">
+                                    <select name="tipo_telefono" id="create_tipo_telefono" class="form-control{{ $errors->has('tipo_telefono') ? ' is-invalid' : '' }}">
+                                        <option value="0" >Seleccione un tipo</option>
+                                        <option value="1" >Celular</option>
+                                        <option value="2" >Fijo</option>
+                                    </select>
+                                    @if ($errors->has('tipo_telefono'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('tipo_telefono') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <input id="create_telefono" type="text" name="telefono" class="form-control{{ $errors->has('telefono') ? ' is-invalid' : '' }}" value="{{old('telefono')}}" placeholder="Número Telefonico">
+                                    @if ($errors->has('telefono'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('telefono') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-sm-12 offset-sm-5">
+                                    <button type="button"  onclick="window.location='{{route('institutos.index')}}'" class="btn btn-secondary">Cancel</button>
+                                    <button type="submit" class="btn btn-primary">Guardar</button>
+                                </div>
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var modelo_departamento = $('#create_departamento');
+            modelo_departamento.change(function(){
+                modelo_departamento.each(function () {
+                    var departamento = modelo_departamento.val();
+
+                    var modelo_municipio = $('#create_municipio');
+                    modelo_municipio.empty();
+                    modelo_municipio.append("<option value='0' >Seleccione un Municipio / Ciudad</option>");
+                    $.ajax({
+                        url: "/institutos/municipios/"+departamento,
+                        type: 'GET'
+                    }).done(function (data) {
+                        $.each(data, function(key, item) {
+                            modelo_municipio.append("<option value='" + item.id + "'>" + item.nombre + "</option>");
+                        });
+                    }).fail( function() {
+                        console.log('Error');
+                    })
+                });
+            });
+        });
+
+    </script>
+@endsection
+
---
+++ resources/views/enfrentamiento/index.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.admin')
+@section('title','Instituciones')
+@section('content')
+    <!-- Breadcrumb-->
+    <div class="breadcrumb-holder container-fluid">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
+            <li class="breadcrumb-item active">Instituciones</li>
+        </ul>
+    </div>
+
+    <!-- Dashboard Counts Section-->
+    <section class="dashboard-counts">
+        <div class="container-fluid">
+            <div class="bg-white has-shadow">
+
+                @if(Session::has('create'))
+                    <script>
+                        setTimeout(function(){
+                                function_swal_confirm('{{Session::get('create')}}', 'creada')
+                        }, 500);
+                    </script>
+                @elseif(Session::has('update'))
+                    <script>
+                        setTimeout(function(){
+                            function_swal_confirm('{{Session::get('update')}}', 'editada')
+                        }, 500);
+                    </script>
+                    @endif
+
+                <div class="row col-sm-8 col-sm-offset-2">
+                    <button onclick="window.location='{{route('equipos.create')}}'" type="button" class="btn btn-info"><span class="fa fa-plus"></span> Nuevo</button>
+                </div>
+                <div class="table-responsive">
+                    <table id="example" class="table table-bordered table-striped table-condensed" style="text-align: center;">
+                        <thead >
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Número de jornada - Fase</th>
+                                <th>Equipo Local</th>
+                                <th>Equipo Visitante</th>
+                                <th>Lugar</th>
+                                
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+    
+                        <tbody >
+                            @foreach($enfrentamientos as $enfrentamiento)
+                                <tr>
+                                    <td>{{$enfrentamiento->calendario->fecha}}</td>
+                                    <td>{{$enfrentamiento->calendario->jornada}} - {{$enfrentamiento->calendario->fase->nombre}}</td>
+                                    <td>{{$enfrentamiento->inscripcion_equipo_local_id}}</td>
+                                    <td>{{$enfrentamiento->inscripcion_equipo_visitante_id}}</td>
+                                    <td>{{$enfrentamiento->lugar->nombre}}</td>
+
+                                    <td>
+                                        <div class="btn-group" role="group" id="action_button">
+                                            <button type="button" class="btn btn-info btn-sm" onclick="window.location='{{route('enfrentamientos.show', $enfrentamiento->id)}}'"><i class="fa fa-eye"></i></button>
+                                            <button type="button" class="btn btn-success btn-sm" onclick="window.location='{{route('enfrentamientos.edit', $enfrentamiento->id)}}'"><i class='fa fa-edit'></i></button>
+    
+                                            <button onclick="id_clickeado({{$enfrentamiento->id}},'{{$enfrentamiento->nombre}}');return function_swal();" class="btn btn-danger btn-sm"><i class='fa fa-trash'></i></button>
+    
+                                            <form action="{{route('enfrentamientos.destroy', $enfrentamiento->id)}}" method="POST">
+                                                {{ method_field('DELETE') }}
+                                                @csrf                       {{-- se le agrega a cada id el de eloquen  --}}
+                                                <button type="submit" id="delete_instituto{{ $enfrentamiento->id }}" style="display: none;"></button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        $(function () {
+            $('#example').DataTable( {
+                "language": {
+                    "url": "{{url('assets/dataTables/Spanish.json')}}"
+                }
+            } );
+        });
+
+        function message(title, text, type) {
+            swal({
+                title: title,
+                text: text,
+                type: type,
+                showCancelButton: false,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Cerrar",
+                closeOnConfirm: true,
+            });
+        }
+
+        var idclick;var nombreclick;
+        function id_clickeado(id,nombre){
+            // console.log("id clickeada => "+id);
+            idclick=id;//captura el id a la cual se le dio click
+            nombreclick=nombre;//captura el nombre a la cual se le dio click
+        }
+
+        function function_swal() {
+            swal({
+                title: "¿Seguro que desea eliminar el enfrentamiento "+nombreclick+"? ",
+                text: "Se eliminara toda la información",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Eliminar",
+                cancelButtonText: "Cancelar",
+                closeOnConfirm: false,
+                closeOnCancel: false },
+
+            function(isConfirm){
+                if (isConfirm) {
+                    swal("Institucion Eliminada!","procesando cambios","success");
+
+                    setTimeout(function(){
+                        var idfinal="#delete_instituto"+idclick;//se le agrega el id que fue clickeado
+                        $(idfinal).click();
+                    }, 500);
+                } else {
+                    swal("Cancelado", "El enfrentamiento NO ha sido eliminado", "error");
+                }
+            });
+        }
+
+        function function_swal_confirm(text, type) {
+            swal("Informacion almacenada", "El enfrentamiento "+text+" ha sido "+type+" correctamente", "success");
+        }
+
+    </script>
+@endsection
