dcc71464b89a51031e4e4d2c8afedbbf0ef5b5c5
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-03-20 17:43:50 -0500

    View enfrentamiento

--- resources/views/enfrentamiento/show.blade.php
+++ resources/views/enfrentamiento/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.admin')
-@section('title','Instituciones')
+@section('title','Enfrentamientos')
 @section('content')
 
     <!-- Breadcrumb-->
     <div class="breadcrumb-holder container-fluid">
         <ul class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
-            <li class="breadcrumb-item active"><a href="{{url('/institutos')}}">Instituciones</a></li>
+            <li class="breadcrumb-item active"><a href="{{url('/enfrentamientos')}}">Enfrentamientos</a></li>
             <li class="breadcrumb-item">Ver</li>
         </ul>
     </div>
@@ -17,73 +17,63 @@
             <div class="bg-white has-shadow">
 
                 <div class="form-group row">
-                    <label for="nombre" class="form-control-label col-sm-3 ">Nombre</label>
-                    <div class="col-sm-9">
-                        <input id="nombre" type="text" name="nit" class="form-control" value="{{$instituto->nombre}}" readonly>
+                    <label for="nombre" class="form-control-label col-sm-3 ">Torneo</label>
+                    <div class="col-sm-4">
+                        <input id="nombre" type="text" name="Torneo" class="form-control" value="{{$enfrentamiento->calendario->torneo->nombre}}" readonly>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="logo" class="form-control-label col-sm-5 ">Logo</label>
-                    <div class="col-sm-6">
-                        <img src="{{asset($instituto->logo)}}" class="rounded-circle" width="250vh">
+                    <label for="nombre" class="form-control-label col-sm-3 ">Fecha</label>
+                    <div class="col-sm-4">
+                        <input id="nombre" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->calendario->fecha}}" readonly>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="nit" class="form-control-label col-sm-3 ">Nit</label>
-                    <div class="col-sm-9">
-                        <input id="nit" type="text" name="nit" class="form-control" value="{{$instituto->nit}}" readonly>
+                    <label for="nombre" class="form-control-label col-sm-3 ">Jornada</label>
+                    <div class="col-sm-4">
+                        <input id="nombre" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->calendario->jornada}}" readonly>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="codigo_dane" class="form-control-label col-sm-3">Codigo Dane</label>
-                    <div class="col-sm-9">
-                        <input id="codigo_dane" type="text" name="codigo_dane" class="form-control" value="{{$instituto->codigo_dane}}" readonly>
-                    </div>
-                </div>
-
-
-                <div class="form-group row">
-                    <label for="departamento" class="col-sm-3 form-control-label">Departamento</label>
-                    <div class="col-sm-9">
-                        <input id="departamento" type="text" name="departamento" class="form-control" value="{{$instituto->municipio->departamento->nombre}}" readonly>
+                    <label for="nombre" class="form-control-label col-sm-3 ">Fase</label>
+                    <div class="col-sm-4">
+                        <input id="nombre" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->calendario->fase->nombre}}" readonly>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="municipio" class="col-sm-3 form-control-label">Municipio / Ciudad</label>
-                    <div class="col-sm-9">
-                        <input id="municipio" type="text" name="municipio" class="form-control" value="{{$instituto->municipio->nombre}}" readonly>
+                    <label for="nombre" class="form-control-label col-sm-3 ">Equipo local</label>
+                    <div class="col-sm-4">
+                        <input id="equipo_local" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->inscripcion_equipo_local->equipo->nombre}}" readonly>
+
+                        <div class="text-center">
+                                <img src="{{asset($enfrentamiento->inscripcion_equipo_local->equipo->logo)}}" class="rounded-circle img-responsive center-block" width="80vh">
+                        </div>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="direccion" class="form-control-label col-sm-3">Direcci&oacute;n</label>
-                    <div class="col-sm-9">
-                        <input id="direccion" type="text" name="direccion" class="form-control" value="Calle {{$instituto->direccion->calle}}, Cra {{$instituto->direccion->carrera}}, #{{$instituto->direccion->numero}}" readonly>
-                    </div>
-                </div>
+                    <label for="nombre" class="form-control-label col-sm-3 ">Equipo Visitante</label>
+                    <div class="col-sm-4">
+                        <input id="equipo_local" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->inscripcion_equipo_visitante->equipo->nombre}}" readonly>
 
-
-                <div class="form-group row">
-                    <label for="tipo_educacion" class="col-sm-3 form-control-label">Tipo de Educacion</label>
-                    <div class="col-sm-9">
-                        <input id="tipo_educacion" type="text" name="tipo_educacion" class="form-control" value="{{$instituto->tipo_educacion->tipo}}" readonly>
+                        <div class="text-center">
+                                <img src="{{asset($enfrentamiento->inscripcion_equipo_visitante->equipo->logo)}}" class="rounded-circle img-responsive center-block" width="80vh">
+                        </div>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="telefono" class="form-control-label col-sm-3">Telefono      </label>
-
-                    <div class="col-sm-3">
-                        <input id="tipo_telefono" type="text" name="tipo_telefono" class="form-control" value="{{$instituto->telefono->tipo}}" readonly>
-                    </div>
-
-                    <div class="col-sm-6">
-                        <input id="telefono" type="text" name="telefono" class="form-control" value="{{$instituto->telefono->numero}}" readonly>
-                    </div>
+                        <label for="nombre" class="form-control-label col-sm-3 ">Lugar</label>
+                        <div class="col-sm-2">
+                            <input id="lugar_nombre" type="text" name="Fecha" class="form-control" value="{{$enfrentamiento->lugar->nombre}}" readonly>
+                        </div>
+                        <div class="col-sm-2">
+                            <input id="lugar_direccion" type="text" name="Fecha" class="form-control" value="Cll {{$enfrentamiento->lugar->direccion->calle}} Crr {{$enfrentamiento->lugar->direccion->carrera}} # {{$enfrentamiento->lugar->direccion->numero}}" readonly>
+                        </div>
                 </div>
 
             </div>
