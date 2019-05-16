4eddb9767f312004243cd5502461904a5ffe7f75
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-05-16 15:47:19 -0500

    Datos Basicos
    CRUD Datos Basicos con estilo de la nueva plantilla

--- resources/views/datosbasicos/index.blade.php
+++ resources/views/datosbasicos/index.blade.php
@@ -1,92 +1,75 @@
 @extends('layouts.admin')
 @section('title','Datos Basicos')
 @section('content')
+@if(Session::has('create'))
+    <script>
+        setTimeout(function(){
+                function_swal_confirm('{{Session::get('create')}}', 'creados')
+        }, 500);
+    </script>
+@elseif(Session::has('update'))
+    <script>
+        setTimeout(function(){
+            function_swal_confirm('{{Session::get('update')}}', 'editados')
+        }, 500);
+    </script>
+@endif
 
-    <!-- Breadcrumb-->
-    <div class="breadcrumb-holder container-fluid">
-        <ul class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
-            <li class="breadcrumb-item active">Datos Basicos</li>
-        </ul>
-    </div>
+<div class="row col-sm-8 col-sm-offset-2 align-left ml-3 mt-3">
+    <button onclick="window.location='{{route('datosbasicos.create')}}'" type="button" class="btn btn-info"><span class="fa fa-plus"></span> Nuevo</button>
+</div>
 
-    <!-- Dashboard Counts Section-->
-    <section class="dashboard-counts">
-        <div class="container-fluid">
-            <div class="bg-white has-shadow">
+<div class="card-body table-responsive">
+    <table id="datosbasicos"  class="table table-striped table-hover card-text" style="text-align: left;">
+        <thead >
+            <tr>
+                <th>Cedula</th>
+                <th>Foto</th>
+                <th>Nombre completo</th>
+                <th>Telefono</th>
+                <th>Municipio</th>
+                <th>Direccion</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
 
-                @if(Session::has('create'))
-                    <script>
-                        setTimeout(function(){
-                                function_swal_confirm('{{Session::get('create')}}', 'creados')
-                        }, 500);
-                    </script>
-                @elseif(Session::has('update'))
-                    <script>
-                        setTimeout(function(){
-                            function_swal_confirm('{{Session::get('update')}}', 'editados')
-                        }, 500);
-                    </script>
-                @endif
-
-                <div class="row col-sm-8 col-sm-offset-2">
-                    <button onclick="window.location='{{route('datosbasicos.create')}}'" type="button" class="btn btn-info"><span class="fa fa-plus"></span> Nuevo</button>
-                </div>
-
-                <div class="table-responsive">
-                    <table id="example" class="table table-bordered table-striped table-condensed" style="text-align: center;">
-                        <thead >
-                            <tr>
-                                <th>Cedula</th>
-                                <th>Foto</th>
-                                <th>Nombre completo</th>
-                                <th>Telefono</th>
-                                <th>Municipio</th>
-                                <th>Direccion</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-
-                        <tbody >
-                        
-                            @foreach($datosbasicos as $datos)
-                                <tr>
-                                    <td>{{ $datos->cedula }}</td>
-                                    <td> <img src="{{asset($datos->foto)}}"  class="mx-auto d-block rounded" width="50vh"></td>
-                                    <td>{{ $datos->primer_nombre }} {{ $datos->segundo_nombre }} {{ $datos->primer_apellido }} {{ $datos->segundo_apellido }}</td>
-                                    <td>@isset($datos->telefono) {{ $datos->telefono->tipo}} {{ $datos->telefono->numero}} @endisset</td>
-                                    <td>{{ $datos->municipio->nombre}}</td>
-                                    <td>Calle {{ $datos->direccion->calle}} Carrera {{ $datos->direccion->carrera}} # {{ $datos->direccion->numero}}</td>
-                                    <td>
-                                        <div class="btn-group" role="group">
-                                            <div class="btn-group" role="group">
-                                                <button type="button" class="btn btn-info btn-sm" onclick="window.location='{{route('datosbasicos.show', $datos->id)}}'"><i class="fa fa-eye"></i></button>
-                                                <button type="button" class="btn btn-success btn-sm" onclick="window.location='{{route('datosbasicos.edit', $datos->id)}}'"><i class='fa fa-edit'></i></button>
+        <tbody >
+        
+            @foreach($datosbasicos as $datos)
+                <tr>
+                    <td>{{ $datos->cedula }}</td>
+                    <td> <img src="{{asset($datos->foto)}}"  class="mx-auto d-block rounded" width="50vh"></td>
+                    <td>{{ $datos->primer_nombre }} {{ $datos->segundo_nombre }} {{ $datos->primer_apellido }} {{ $datos->segundo_apellido }}</td>
+                    <td>@isset($datos->telefono) {{ $datos->telefono->tipo}} {{ $datos->telefono->numero}} @endisset</td>
+                    <td>{{ $datos->municipio->nombre}}</td>
+                    <td>Calle {{ $datos->direccion->calle}} Carrera {{ $datos->direccion->carrera}} # {{ $datos->direccion->numero}}</td>
+                    <td>
+                        <div class="btn-group" role="group">
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-info btn-sm" onclick="window.location='{{route('datosbasicos.show', $datos->id)}}'"><i class="fa fa-eye"></i></button>
+                                <button type="button" class="btn btn-success btn-sm" onclick="window.location='{{route('datosbasicos.edit', $datos->id)}}'"><i class='fa fa-edit'></i></button>
 
 
-                                                <button onclick="id_clickeado({{ $datos->id }},'{{ $datos->primer_nombre }} {{ $datos->segundo_nombre }} {{ $datos->primer_apellido }} {{ $datos->segundo_apellido }}');return function_swal();" class="btn btn-danger btn-sm"><i class='fa fa-trash'></i></button>
-                                                
-                                                <form action="{{route('datosbasicos.destroy', $datos->id)}}" method="POST">
-                                                    {{ method_field('DELETE') }}
-                                                    @csrf
-                                                    <button type="submit" id="delete_datos_basicos{{ $datos->id }}" style="display: none;"></button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </section>
+                                <button onclick="id_clickeado({{ $datos->id }},'{{ $datos->primer_nombre }} {{ $datos->segundo_nombre }} {{ $datos->primer_apellido }} {{ $datos->segundo_apellido }}');return function_swal();" class="btn btn-danger btn-sm"><i class='fa fa-trash'></i></button>
+                                
+                                <form action="{{route('datosbasicos.destroy', $datos->id)}}" method="POST">
+                                    {{ method_field('DELETE') }}
+                                    @csrf
+                                    <button type="submit" id="delete_datos_basicos{{ $datos->id }}" style="display: none;"></button>
+                                </form>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
 @section('scripts')
     <script type="text/javascript">
         $(function () {
-            $('#example').DataTable( {
+            $('#datosbasicos').DataTable( {
                 "language": {
                     "url": "{{url('assets/dataTables/Spanish.json')}}"
                 }
