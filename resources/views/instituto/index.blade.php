e2dcd6d38adc13c34882d09ff1d5302eec437f7b
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-05-16 16:42:53 -0500

    Institutos
    CRUD Institutos con estilo de la nueva plantilla.

--- resources/views/instituto/index.blade.php
+++ resources/views/instituto/index.blade.php
@@ -1,82 +1,66 @@
 @extends('layouts.admin')
-@section('title','Instituciones')
+@section('title','Listado De Instituciones')
 @section('content')
-    <!-- Breadcrumb-->
-    <div class="breadcrumb-holder container-fluid">
-        <ul class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{url('/home')}}">Inicio</a></li>
-            <li class="breadcrumb-item active">Instituciones</li>
-        </ul>
-    </div>
+@if(Session::has('create'))
+    <script>
+        setTimeout(function(){
+                function_swal_confirm('{{Session::get('create')}}', 'creada')
+        }, 500);
+    </script>
+@elseif(Session::has('update'))
+    <script>
+        setTimeout(function(){
+            function_swal_confirm('{{Session::get('update')}}', 'editada')
+        }, 500);
+    </script>
+    @endif
 
-    <!-- Dashboard Counts Section-->
-    <section class="dashboard-counts">
-        <div class="container-fluid">
-            <div class="bg-white has-shadow">
+<div class="row col-sm-8 col-sm-offset-2 align-left ml-3 mt-3">
+    <button onclick="window.location='{{route('institutos.create')}}'" type="button" class="btn btn-info"><span class="fa fa-plus"></span> Nuevo</button>
+</div>
+<div class="card-body table-responsive">
+    <table id="instituciones" class="table table-striped table-hover card-text" style="text-align: center;">
+        <thead >
+            <tr>
+                <th>Codigo dane</th>
+                <th>Nit</th>
+                <th>Nombre</th>
+                <th>Logo</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
 
-                @if(Session::has('create'))
-                    <script>
-                        setTimeout(function(){
-                                function_swal_confirm('{{Session::get('create')}}', 'creada')
-                        }, 500);
-                    </script>
-                @elseif(Session::has('update'))
-                    <script>
-                        setTimeout(function(){
-                            function_swal_confirm('{{Session::get('update')}}', 'editada')
-                        }, 500);
-                    </script>
-                    @endif
+        <tbody >
+            @foreach($institutos as $instituto)
+                <tr>
+                    <td>{{$instituto->codigo_dane}}</td>
+                    <td>{{$instituto->nit}}</td>
+                    <td>{{$instituto->nombre}}</td>
+                    <td> <img src="{{asset($instituto->logo)}}"  class="img-fluid rounded-circle" width="50vh"></td>
+                    <td>
+                        <div class="btn-group" role="group" id="action_button">
+                            <button type="button" class="btn btn-info btn-sm" onclick="window.location='{{route('institutos.show', $instituto->id)}}'"><i class="fa fa-eye"></i></button>
+                            <button type="button" class="btn btn-success btn-sm" onclick="window.location='{{route('institutos.edit', $instituto->id)}}'"><i class='fa fa-edit'></i></button>
 
-                <div class="row col-sm-8 col-sm-offset-2">
-                    <button onclick="window.location='{{route('institutos.create')}}'" type="button" class="btn btn-info"><span class="fa fa-plus"></span> Nuevo</button>
-                </div>
-                <div class="table-responsive">
-                    <table id="example" class="table table-bordered table-striped table-condensed" style="text-align: center;">
-                        <thead >
-                            <tr>
-                                <th>Codigo dane</th>
-                                <th>Nit</th>
-                                <th>Nombre</th>
-                                <th>Logo</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-    
-                        <tbody >
-                            @foreach($institutos as $instituto)
-                                <tr>
-                                    <td>{{$instituto->codigo_dane}}</td>
-                                    <td>{{$instituto->nit}}</td>
-                                    <td>{{$instituto->nombre}}</td>
-                                    <td> <img src="{{asset($instituto->logo)}}"  class="img-fluid rounded-circle" width="50vh"></td>
-                                    <td>
-                                        <div class="btn-group" role="group" id="action_button">
-                                            <button type="button" class="btn btn-info btn-sm" onclick="window.location='{{route('institutos.show', $instituto->id)}}'"><i class="fa fa-eye"></i></button>
-                                            <button type="button" class="btn btn-success btn-sm" onclick="window.location='{{route('institutos.edit', $instituto->id)}}'"><i class='fa fa-edit'></i></button>
-    
-                                            <button onclick="id_clickeado({{$instituto->id}},'{{$instituto->nombre}}');return function_swal();" class="btn btn-danger btn-sm"><i class='fa fa-trash'></i></button>
-    
-                                            <form action="{{route('institutos.destroy', $instituto->id)}}" method="POST">
-                                                {{ method_field('DELETE') }}
-                                                @csrf                       {{-- se le agrega a cada id el de eloquen  --}}
-                                                <button type="submit" id="delete_instituto{{ $instituto->id }}" style="display: none;"></button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </section>
+                            <button onclick="id_clickeado({{$instituto->id}},'{{$instituto->nombre}}');return function_swal();" class="btn btn-danger btn-sm"><i class='fa fa-trash'></i></button>
+
+                            <form action="{{route('institutos.destroy', $instituto->id)}}" method="POST">
+                                {{ method_field('DELETE') }}
+                                @csrf                       {{-- se le agrega a cada id el de eloquen  --}}
+                                <button type="submit" id="delete_instituto{{ $instituto->id }}" style="display: none;"></button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
 @section('scripts')
     <script type="text/javascript">
         $(function () {
-            $('#example').DataTable( {
+            $('#instituciones').DataTable( {
                 "language": {
                     "url": "{{url('assets/dataTables/Spanish.json')}}"
                 }
