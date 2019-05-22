b022c55706673189584ac563efdca7dbf93b5bd5
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-05-21 21:03:41 -0500

    Trigger para el calculo de puntos incluido en la Dump

--- database/dump/trigger.sql
+++ database/dump/trigger.sql
@@ -10,16 +10,36 @@ begin
 	declare id_equipo_visitante int;
 	declare resultado_visitante int;
 
-	SELECT resultado.resultado_local  INTO resultado_local, enfrentamiento.inscripcion_equipo_local_id INTO id_equipo_local FROM enfrentamiento 
+-- Equipo Local
+	SELECT resultado.resultado_local  INTO resultado_local FROM enfrentamiento 
     INNER JOIN resultado ON enfrentamiento.id = resultado.enfrentamiento_id  WHERE enfrentamiento.id = new.id;
 
-	-- SELECT resultado.equipo_visitante  INTO equipo_visitante, enfrentamiento.inscripcion_equipo_visitante_id INTO id_equipo_visitante FROM enfrentamiento
-	-- INNER JOIN resultado ON enfrentamiento.id = resultado.enfrentamiento_id 
-	-- WHERE enfrentamiento.id = new.id;
+	SELECT enfrentamiento.inscripcion_equipo_local_id INTO id_equipo_local FROM enfrentamiento 
+    INNER JOIN resultado ON enfrentamiento.id = resultado.enfrentamiento_id  WHERE enfrentamiento.id = new.id;
+
+-- Equipo Visitante
+	SELECT resultado.resultado_visitante  INTO resultado_visitante FROM enfrentamiento 
+    INNER JOIN resultado ON enfrentamiento.id = resultado.enfrentamiento_id  WHERE enfrentamiento.id = new.id;
+
+	SELECT enfrentamiento.inscripcion_equipo_visitante_id INTO id_equipo_visitante FROM enfrentamiento 
+    INNER JOIN resultado ON enfrentamiento.id = resultado.enfrentamiento_id  WHERE enfrentamiento.id = new.id;
+
 
     IF resultado_local > resultado_visitante THEN
-        UPDATE inscripcion_equipo SET puntos = puntos + 3
-    WHERE inscripcion_equipo.id = id_equipo_local; 
+			UPDATE inscripcion_equipo SET puntos = puntos + 3
+			WHERE inscripcion_equipo.id = id_equipo_local;
+
+			ELSEIF resultado_visitante > resultado_local THEN
+				UPDATE inscripcion_equipo SET puntos = puntos + 3
+				WHERE inscripcion_equipo.id = id_equipo_visitante;
+
+				ELSEIF resultado_local = resultado_visitante THEN
+					UPDATE inscripcion_equipo SET puntos = puntos + 1 WHERE inscripcion_equipo.id = id_equipo_local; 
+					UPDATE inscripcion_equipo SET puntos = puntos + 1 WHERE inscripcion_equipo.id = id_equipo_visitante; 
     END IF;
 end //
 delimiter ;
+
+
+
+DROP TRIGGER puntos_enfrentamiento;
